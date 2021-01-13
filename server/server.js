a2e719571a52b7efa7d8db155a8cb53c6109395b
Kevin Walton <kevinwalton@kevins-mbp.lan>
2021-01-12 22:54:27 -0800

    add crud features

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import config from '../config/config';
+import app from './express';
+import mongoose from 'mongoose';
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+console.log(config.mongoUri);
+
+mongoose.connection.on('error', () => {
+    throw new Error(`unable to connect to database: ${mongoUri}`);
+});
+
+app.listen(config.port, (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.info('server started on port %s.', config.port);
+});
